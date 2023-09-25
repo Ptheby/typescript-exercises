11e6dd6dd58ea3fd74267ec8bd9721574835a49c
Paul Theby <paultheby@gmail.com>
2023-09-24 23:10:47 -0500

    Exercise-3

---
+++ Exercise-3/3.ts
@@ -0,0 +1,26 @@
+interface Vehicle {
+    brand: string;
+    speed: number;
+    
+    accelerate(): void;
+    brake(): void;
+}
+
+const car: Vehicle = {
+    brand: "Toyota",
+    speed: 120,
+    accelerate(): void {
+        console.log(`The ${this.brand} is accelerating`);
+    },
+    brake(): void {
+        console.log(`The ${this.brand} is braking`);
+    }
+}
+
+car.accelerate();
+car.brake();
+
+
+
+
+//Use the above interface and object. Add another method in the interface called brake and implement it in the car object.
