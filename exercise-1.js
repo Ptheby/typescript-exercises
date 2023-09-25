8782cf54415dde7b7bee883bdc360a27e3256f60
Paul Theby <paultheby@gmail.com>
2023-09-24 22:41:26 -0500

    Exercise-1

---
+++ exercise-1.js
@@ -0,0 +1,6 @@
+var studentName;
+studentName = "Paul Theby";
+var totalCourses;
+totalCourses = 23;
+console.log(studentName);
+console.log(totalCourses);
---
+++ exercise-1.ts
@@ -0,0 +1,7 @@
+let studentName: string;
+studentName= "Paul Theby";
+
+let totalCourses: number;
+totalCourses= 23; 
+
+console.log(studentName);
